abf814224d68719050e354c2a6d0b14d68fead92
MohammadBohlak <abuomarcom4@gmail.com>
2025-02-27 20:56:13 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./pages/home/Home";
 import GlobalStyles from "./styles/GlobalStyles";
-import { createBrowserRouter, Route, Router, RouterProvider, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  Route,
+  Router,
+  RouterProvider,
+  Routes,
+} from "react-router-dom";
 import About from "./pages/about/About";
 import Contact from "./pages/Contact";
 import NavBar from "./components/common/navbar/NavBar";
 import Footer from "./components/common/footer/Footer";
 import { HeadProvider } from "react-head";
-
 
 function App() {
   return (
@@ -16,7 +22,7 @@ function App() {
       <HeadProvider>
         <GlobalStyles />
         <NavBar />
-        {/* <RouterProvider router={router} /> */}
+        
         <Footer />
       </HeadProvider>
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Router from "./Router.jsx";
+import Home from "./pages/home/Home.jsx";
+import About from "./pages/about/About.jsx";
+import Contact from "./pages/Contact.jsx";
 
-createRoot(document.getElementById("root")).render(<Router />);
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  </BrowserRouter>
+);
